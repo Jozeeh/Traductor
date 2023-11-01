5884b3c74956df4006c899d6021124c5f9133ed9
José Mejia <ja3149939@gmail.com>
2023-10-31 18:53:57 -0600

    Cambio de la apiKey y desactivación temporalmente de subir de store/index.js en .gitignore

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,7 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
-    apiKey: '2d32e956f7msh1d275f158bbd1f0p151c69jsn3fffe9d6a4e1'
+    apiKey: '88cc51c7a8msh5cb26f3451f98aep13c7f7jsnd91cfddc6d9c'
 
   },
   getters: {
