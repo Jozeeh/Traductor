f921b52d9a8e9f336149d99ef6011b3cc8e8a9ff
José Mejia <josemejia.cto@gmail.com>
2023-11-04 23:04:15 -0600

    [CAMBIOS] 1. Se agrego el redireccionamiento de registro según la sesión, 2. El registro ahora es completamente funcional, 3. Variable en el state de store/index.js para guardar los datos de la sesión

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,12 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
-    apiKey: '88cc51c7a8msh5cb26f3451f98aep13c7f7jsnd91cfddc6d9c'
+    // Descomentar la api que usaras
+    //ipApi: 'http://localhost:8000/api', //Api local
+    ipApi: 'https://apitr.query.lat/public/api', //Api de produccion (En servidor)
+    apiKey: '88cc51c7a8msh5cb26f3451f98aep13c7f7jsnd91cfddc6d9c',
+
+    datosUsuario: []
 
   },
   getters: {
--- src/views/sesion-views/RegistroPage.vue
+++ src/views/sesion-views/RegistroPage.vue
@@ -2,21 +2,24 @@
     <v-grid>
         <v-row style="margin-top: 10px;" justify="center">
             <v-col :cols="12" :sm="12" :md="6" :lg="6" :xl="6" :xxl="6">
-                <v-card :loading="loadingRegistro" color="grey-lighten-3" variant="flat" class="mx-auto" prepend-icon="mdi-account-plus">
+                <v-card :loading="loadingRegistro" color="grey-lighten-3" variant="flat" class="mx-auto"
+                    prepend-icon="mdi-account-plus">
                     <template v-slot:title>
                         Registra tu cuenta!
                         <v-card-subtitle>¡Al registrarte podrás acceder a más beneficios!</v-card-subtitle>
                     </template>
 
                     <v-card-text>
-                        <v-text-field label="Nombre" outlined></v-text-field>
-                        <v-text-field label="Apellido" outlined></v-text-field>
-                        <v-text-field type="number" label="Número telefonico" outlined></v-text-field>
-                        <v-text-field label="Correo" outlined></v-text-field>
-                        <v-text-field type="password" label="Contraseña" outlined></v-text-field>
-                        <v-text-field type="password" label="Confirmar contraseña" outlined></v-text-field>
-                        
-                        <v-btn color="brown" @click="loadingRegistro=true">Registrarse</v-btn>
+                        <v-text-field :disabled="desactivarFormulario" label="Nombre" outlined v-model="cuentaRegistrar.nombre"></v-text-field>
+                        <v-text-field :disabled="desactivarFormulario" label="Apellido" outlined v-model="cuentaRegistrar.apellido"></v-text-field>
+                        <v-text-field :disabled="desactivarFormulario" type="number" label="Número telefonico" outlined
+                            v-model="cuentaRegistrar.telefono"></v-text-field>
+                        <v-text-field :disabled="desactivarFormulario" label="Correo" outlined v-model="cuentaRegistrar.correo"></v-text-field>
+                        <v-text-field :disabled="desactivarFormulario" type="password" label="Contraseña" outlined v-model="cuentaRegistrar.password"></v-text-field>
+                        <v-text-field :disabled="desactivarFormulario" type="password" label="Confirmar contraseña" outlined
+                            v-model="passwordConfirm"></v-text-field>
+
+                        <v-btn :disabled="desactivarFormulario" color="brown" @click="registrarCuenta()">Registrarse</v-btn>
                     </v-card-text>
                 </v-card>
             </v-col>
@@ -27,21 +30,149 @@
                 <v-card color="blue-grey-lighten-4" variant="flat" theme="dark">
                     <template v-slot:text>
                         <div class="text-h6">¡¿CONOCIAS LOS BENEFICIOS QUE OBTIENES AL REGISTRARTE?!</div>
-                        Puedes acceder a los siguientes al beneficios al registrarte, como por ejemplo: acceder a un diccionario en línea y poder encontrar sinonimos y antonimos de palabras que necesites!
+                        Puedes acceder a los siguientes al beneficios al registrarte, como por ejemplo: acceder a un
+                        diccionario en línea y poder encontrar sinonimos y antonimos de palabras que necesites!
                     </template>
                 </v-card>
             </v-col>
         </v-row>
+
+        <!-- SNACK DE AVISO -->
+        <v-snackbar v-model="snackAviso">
+            <div v-text="snackAvisoTexto"></div>
+
+            <template v-slot:actions>
+                <v-btn color="pink" variant="text" @click="snackContraseñaDiferente = false">
+                    cerrar
+                </v-btn>
+            </template>
+        </v-snackbar>
+
+        <!-- Dialog que se muestra cuando está cargando el registro de usuario -->
+        <v-dialog v-model="dialogCargandoRegistro" fullscreen>
+            <v-card class="align-center justify-center">
+                <v-progress-circular color="primary" indeterminate :size="128" :width="12"></v-progress-circular>
+                <h1>Iniciando sesión...</h1>
+            </v-card>
+        </v-dialog>
     </v-grid>
 </template>
 
 
 <script>
+import axios from 'axios'
+
 export default {
     name: 'RegistroPage',
     data() {
         return {
-            loadingRegistro: false
+            cuentaRegistrar: {
+                nombre: '',
+                apellido: '',
+                telefono: '',
+                correo: '',
+                password: '',
+            },
+            passwordConfirm: '',
+            loadingRegistro: false,
+            snackAviso: false,
+            snackAvisoTexto: '',
+            desactivarFormulario: false,
+            dialogCargandoRegistro: false
+        }
+    },
+    methods: {
+        // Método para iniciar sesión despues de registrarse
+        iniciarSesion(){
+            axios.post(`${this.$store.state.ipApi}/login`, {
+                correo: this.cuentaRegistrar.correo,
+                password: this.cuentaRegistrar.password
+            })
+            .then(response => {
+                localStorage.setItem('datosSesion', JSON.stringify(response.data.data))
+                this.$router.push('/home')
+            })
+            .catch(error => {
+                console.log(error)
+            })
+        },
+
+        // Método para registrar una cuenta
+        registrarCuenta() {
+            // Mostrar el dialog de cargando registro
+            this.dialogCargandoRegistro = true
+
+            // Si todos los campos están llenos se ejecuta
+            if (this.cuentaRegistrar.nombre != '' || this.cuentaRegistrar.apellido != '' || this.cuentaRegistrar.telefono != '' || this.cuentaRegistrar.correo != '' || this.cuentaRegistrar.password != '' || this.passwordConfirm != ''){
+
+               // Si las contraseñas no coinciden mostrará un aviso
+                if (this.cuentaRegistrar.password != this.passwordConfirm) {
+                    // Texto del Snackbar
+                    // Mostrar el Snackbar
+                    this.snackAvisoTexto = '¡Las contraseñas no coinciden!'
+                    this.snackAviso = true
+
+                    // Ocultar el Snackbar después de unos segundos (por ejemplo, 3 segundos <<3000 milisegundos (3 segundos)>>)
+                    setTimeout(() => {
+                        this.snackAviso = false;
+                    }, 3000);
+
+                }
+                //Si las contraseñas coinciden se procede a registrar la cuenta 
+                else {
+                    // Desactivamos el formulario y mostramos el loading
+                    this.desactivarFormulario = true
+                    this.loadingRegistro = true
+                    
+                    // Se envía la petición al servidor
+                    axios.post(`${this.$store.state.ipApi}/registro`, this.cuentaRegistrar)
+                    .then(response => {
+                        //Mensaje y datos obtenido del registro exitoso
+                        console.log('Registro exitoso' + response.data.data)
+                        
+                        // Si el registro es exitoso se inicia sesión
+                        this.iniciarSesion()
+
+                        // Ocultamos el loading y el dialog
+                        this.dialogCargandoRegistro = false
+                    })
+                    .catch(error => {
+                        // Texto del Snackbar
+                        this.snackAvisoTexto = 'Ya existe una cuenta con esté correo!'
+                        // Mostrar el Snackbar
+                        this.snackAviso = true
+
+                        // Ocultar el Snackbar después de unos segundos (por ejemplo, 3 segundos)
+                        setTimeout(() => {
+                            this.snackAviso = false;
+                        }, 3000); // 3000 milisegundos (3 segundos)
+
+                        // Imprimimos error en consola
+                        console.log('Error al registrar' + error)
+                    })
+
+                } 
+            } 
+            // Si todos los campos NO están llenos se ejecuta
+            else{
+                // Texto del Snackbar
+                this.snackAvisoTexto = '¡Debes llenar todos los campos!'
+                // Mostrar el Snackbar
+                this.snackAviso = true
+
+                // Ocultar el Snackbar después de unos segundos (por ejemplo, 3 segundos)
+                setTimeout(() => {
+                    this.snackContraseñaDiferente = false;
+                }, 3000); // 3000 milisegundos (3 segundos)
+            }
+        }
+    },
+    beforeCreate() {
+        // Comprobamos si hay datos de sesion guardados en el store
+        if (this.$store.state.datosUsuario.length != 0) {
+            this.$router.push('/perfil/ajustes')
+        } else {
+            console.log('Registrate!')
         }
     }
 }
